d1405c80ed9f0cae5396d782369799ccbd61cbf5
Deepak Parashar <143609163+weblydeepak@users.noreply.github.com>
2024-09-29 16:11:43 +0530

    update

--- src/Components/Audio/AudioPlayer.js
+++ src/Components/Audio/AudioPlayer.js
@@ -3,127 +3,36 @@ import ProgressCircle from './ProgressCircle'
 import WaveAnimation from './WaveAnimation'
 // import Controls from './Controls'
 import  '../../Styles/AudioPlayer.css'
-const AudioPlayer = ({currentTracks , currentIndex,setCurrentIndex, total}) => {
-  // console.log(currentTracks?.artists);
+const AudioPlayer = ({currentTracks , currentIndex, setCurrentIndex, total}) => {
+ 
+  const audioRed= useRef(null);
+  const [isPlaying, setIsPlaying]=useState(false);
   
-  const [isPlaying, setIsPlaying]= useState(true)
-  const [trackProgress, setTrackProgress]= useState(0)
-  var audioSrc = total[currentIndex]?.track.preview_url;
-  
-  const audioRef = useRef(new Audio(total[0]?.track.preview_url));
-  // const intervalRef = useRef(null);
-  // const isReady = useRef(false)
-
-  const {duration}= audioRef.current;
-
-
-  const currentPercecntage= duration?(trackProgress/duration) *100:0;
-
-  
-
-  // console.log(audioRef.current);
-  //  const {duration} = audioRef.current;
-   
-
-  //  const currentPercecntage =  duration?(trackProgress/duration) *100:0;
-   
-  //  const startTimer=()=>{
-  //   clearInterval(intervalRef.current);
-  //   intervalRef.current = setInterval(()=>{
-  //   if(audioRef.current.ended){
-  //     handleNext();
-  //   }
-  //   else{
-  //     setTrackProgress(audioRef.current.currentTime);
-  //   }
-  //   },[1000])
-  //  }
-   
-  //  useEffect(() => {
-  //   if (audioRef.current.src) {
-  //     if (isPlaying) {
-  //       audioRef.current.play();
-  //       startTimer();
-  //     } else {
-  //       clearInterval(intervalRef.current);
-  //       audioRef.current.pause();
-  //     }
-  //   } else {
-  //     if (isPlaying) {
-  //       audioRef.current = new Audio(audioSrc);
-  //       audioRef.current.play();
-  //       startTimer();
-  //     } else {
-  //       clearInterval(intervalRef.current);
-  //       audioRef.current.pause();
-  //     }
-  //   }
-  // }, [isPlaying]);
-  //  useEffect(()=>{
-  //  audioRef.current.pause();
-  //  audioRef.current= new Audio(audioSrc);
-  //  setTrackProgress(audioRef.current.currentTime)
-  //  if(isReady.current){
-  //    audioRef.current.play();
-  //    setIsPlaying(true);
-  //    startTimer();
-  //  }else{
-  //   isReady.current = true;
-  //  }
-  //  },[currentIndex])
-   
-
-  //  useEffect(()=>{
-  //   return()=>{
-  //     audioRef.current.pause();
-  //     clearInterval(intervalRef.current);
-  //   };
-  //  },[])
+ const togglerPlayPause=()=>{
+ 
+   setIsPlaying(!isPlaying);
+   if (isPlaying) {
+     audioRed.current.pause();
+   } else {
+     audioRed.current.play();
+   }
+ }
 
 
-  //  const handleNext = ()=>{
-  //   if(currentIndex<total.length-1){
-  //     setCurrentIndex(currentIndex+1);
-  //   }
-  //   else{
-  //     setCurrentIndex(0);
-  //   }
-  //  };
-   
-  //  const handlePrev = ()=>{
-  //   if(currentIndex -1<0){
-  //     setCurrentIndex(currentIndex-1);
-  //   }
-  //   else{
-  //     setCurrentIndex(currentIndex-1);
-  //   }
-  //  }
-
-   const addZero=(n)=>{
-    return n>9?""+n:"0"+n;
-   };
-
-  const artists=[]
+const artists = [];
   if(currentTracks!==undefined ){
-     currentTracks.artists.map((artist)=> artists.push(artist.name));
+    currentTracks.artists.map((artist)=> artists.push(artist.name));
   }
-  
-  
 
-useEffect(()=>{
-  if(artists){
-  console.log("artist",artists);
-  }
- },[artists]
-)
+
 
   return (
    <div className="PlayerBody flex">
     <div className="playerLeftBody">
    {currentTracks&& <ProgressCircle
-          percentage={currentPercecntage}
+          percentage={70}
           isPlaying={true}
-          image={currentTracks?.images[0]?.url}
+          image={curretTracks?.images[0]?.url}
           size={300}
           color="#C96850"
         />}
@@ -135,11 +44,15 @@ useEffect(()=>{
       <p className='songArtist'>{artists.join("|")}</p>
       <div className="playerRightBottom flex">
         <div className="songDuration flex">
-          <p className="duration">0:{addZero(Math.round(trackProgress))}</p>
+          {/* <p className="duration">0:{addZero(Math.round(trackProgress))}</p> */}
+          <p className="duration">0</p>
           <WaveAnimation isPlaying={true} />
           <p className="duration">0.30</p>
         </div>
 
+        {/* <div className="conttroler">
+          <button onClick={playHandle}>{isPlaying?"play":'pause'}</button>
+        </div> */}
 
           {/* <Controls
           isPlaying={isPlaying}
